7c03da10a21a24676c564b0cc834335738eb9adb
atsumaru <116328636+atsumaru307@users.noreply.github.com>
2024-10-14 15:36:21 +0900

    Add files via upload

---
+++ flask-blog/routes/API.py
@@ -0,0 +1,172 @@
+from flask import Blueprint, jsonify, request
+import sys
+sys.path.append("../")
+from database import Engine, create_session, Article
+
+router = Blueprint("API", __name__, url_prefix="/api")
+
+#エラーが出たときの処理
+class ArticlePostValueError(Exception):
+    pass
+
+class ArticleNotFoundError(Exception):
+    pass
+
+@router.route("/post", methods=["POST"])
+def post():
+    try:
+        title = request.form.get("title", None, type=str)
+        body = request.form.get("body", None, type=str)
+
+        if (title is None) or (body is None) or (title == "") or (body == ""):
+            raise ArticlePostValueError("タイトルと本文を入力してください")
+        
+        if len(title) > 255:
+            raise ArticlePostValueError("タイトルの文字数が255文字を超えています。")
+                        
+        session = create_session() #データベースに接続
+        article = Article(
+            title=title,
+            body=body
+        )
+        session.add(article) #追加
+        session.commit() #反映
+        
+        return jsonify({
+            "result": True
+        })
+    except ArticlePostValueError as e:
+        print(e)
+        return jsonify({
+            "result": False,
+            "message": e.args[0]
+        }), 400
+    except Exception as e:
+        print(e)
+        return jsonify({
+            "result": False,
+            "message": "Internal Server Error"
+        }), 500
+    
+@router.route("/articles")
+def get_articles():
+    try:
+        session = create_session()
+        articles = session.query(Article).all()
+        return jsonify([article.to_dict() for article in articles])
+    except:
+        return jsonify([]), 500
+
+@router.route("/article/<int:_id>")
+def get_article(_id):
+    try:
+        session = create_session()
+        article = session.query(Article).filter(Article.id == _id).first()
+        if article is None:
+            raise ArticleNotFoundError("記事が存在しません")
+        return jsonify({
+            "result": True,
+            "article": article.to_dict()
+        })
+    except ArticleNotFoundError as e:
+        print(e)
+        return jsonify({
+            "result": False,
+            "message": "記事が存在しません"
+        }), 404
+    except Exception as e:
+        print(e)
+        return jsonify({
+            "result": False,
+            "message": "Internal Server Error"
+        }), 500
+    
+@router.route("/update", methods=["POST"])
+def update():
+    try:
+        title = request.form.get("title", None, type=str)
+        body = request.form.get("body", None, type=str)
+        _id = request.form.get("id", None, type=int)
+
+        if _id is None:
+            raise ArticlePostValueError("idを入力してください")
+
+        if (title is None) and (body is None) or (title == "") and (body == ""):
+            raise ArticlePostValueError("タイトルと本文を入力してください")
+
+        if title:
+            if len(title) > 255:
+                raise ArticlePostValueError("タイトルの文字数が255文字を超えています。")
+        
+        
+        session = create_session()
+        article = session.query(Article).filter(Article.id == _id).first()
+        if article is None:
+            raise ArticleNotFoundError("記事が見つかりません")
+        
+        if title:
+            article.title = title
+        if body:
+            article.body = body
+        session.commit()
+
+        return jsonify({
+            "result": True
+        })
+    except ArticlePostValueError as e:
+        print(e)
+        return jsonify({
+            "result": False,
+            "message": e.args[0]
+        }), 400
+    except ArticleNotFoundError as e:
+        print(e)
+        return jsonify({
+            "result": False,
+            "message": e.args[0]
+        }), 404
+    except Exception as e:
+        print(e)
+        return jsonify({
+            "result": False,
+            "message": "Internal Server Error"
+        }), 500
+    
+@router.route("/delete", methods=["DELETE"])
+def delete():
+    try:
+        _id = request.form.get("id", None, type=int)
+
+        if _id is None:
+            raise ArticlePostValueError("IDを入力してください")
+
+        session = create_session()
+        article = session.query(Article).filter(Article.id == _id).first()
+        if article is None:
+            raise ArticleNotFoundError("記事が見つかりません")
+        
+        session.delete(article)
+        session.commit()
+
+        return jsonify({
+            "result": True
+        })
+
+    except ArticlePostValueError as e:
+        print(e)
+        return jsonify({
+            "result": False,
+            "message": e.args[0]
+        }), 400
+    except ArticleNotFoundError as e:
+        print(e)
+        return jsonify({
+            "result": False,
+            "message": e.args[0]
+        }), 404
+    except Exception as e:
+        print(e)
+        return jsonify({
+            "result": False,
+            "message": "Internal Server Error"
+        }), 500
---
+++ flask-blog/static/js/index.js
@@ -0,0 +1,76 @@
+function request(url, options){
+    if(!options){
+        options = {}
+    }
+    return fetch(url, options)
+        .then(res => res.json())
+        .then(data => {
+            return data
+        })
+        .catch(err => {
+            console.log(err)
+            return {
+                result: false,
+                message: err.message
+            }
+        })
+}
+
+function getArticles(){
+    return request('/api/articles')
+}
+
+function getArticle(id){
+    return request('/api/article/' + id)
+}
+
+function postArticle(title, body){
+    var params = new URLSearchParams()
+    params.append('title', title)
+    params.append('body', body)
+    return request('/api/post', {
+        method: 'POST',
+        body: params
+    })
+}
+
+function updateArticle(id, title, body){
+    var params = new URLSearchParams()
+    params.append("id", id)
+    if(title){
+        params.append('title', title)
+    }
+    if(body){
+        params.append('body', body)
+    }
+    return request('/api/update', {
+        method: 'POST',
+        body: params
+    })
+}
+
+function deleteArticle(id){
+    var params = new URLSearchParams()
+    params.append('id', id)
+    return request('/api/delete', {
+        method: 'DELETE',
+        body: params
+    })
+}
+
+function toDateString(time) {
+    var date = new Date(time * 1000)
+    date.setHours(date.getHours() + 9)
+    return date.getFullYear() + "/" + (date.getMonth() + 1) + "/" + date.getDate();
+}
+
+function parseQuery(){
+    var query = location.search.substring(1)
+    var params = query.split("&")
+    var result = {}
+    for(var param of params){
+        var key_value = param.split("=")
+        result[key_value[0]] = key_value[1]
+    }
+    return result
+}
